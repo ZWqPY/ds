29579a3ae3b3243d7b0329c2de4518c47afdc767
ZWqPY <78861832+ZWqPY@users.noreply.github.com>
2021-02-10 19:01:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ import asyncio
 from config import settings
 import json
 
+settings = {
+    'token': 'ODAyOTQ1OTc4MDQ2ODczNjcy.YA2ntw.0ntrS1rHnvOgd7UO3JxNvIuNgPE',
+    'bot': 'python',
+    'id': 802945978046873672,
+    'prefix': '!'
+}
+
 client = discord.Client()
 bot = commands.Bot(command_prefix = settings['prefix'])
 
